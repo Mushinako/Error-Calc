e825ad38751a9207e85206de941a29d875a98863
Mushinako <ridoedee@gmail.com>
2020-02-13 13:08:00 -0800

    Enlarge base icon

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,9 @@
 "use strict";
-const cacheName = 'mushinako-err-prop-v1.1.1';
+const cacheName = 'mushinako-err-prop-v1.1.2';
 const filesToCache = [
     'index.html',
     'css/common.css',
     'css/index.css',
-    'images/icons/icon-96x96.png',
-    'images/icons/icon-144x144.png',
-    'images/icons/icon-192x192.png',
-    'images/icons/icon-256x256.png',
     'images/icons/icon-512x512.png',
     'images/icons/icon-768x768.png',
     'images/icons/icon-1000x1000.png',
@@ -26,11 +22,6 @@ self.addEventListener('install', (e) => {
         caches.open(cacheName).then((cache) => {
             console.log('[ServiceWorker] Pre-caching offline page');
             return cache.addAll(filesToCache);
-            // return cache.addAll(filesToCache.map((url) => new Request(url, {
-            //     mode: 'no-cors'
-            // }))).then(() => {
-            //     console.log('[ServiceWorker] Request finished!');
-            // });
         })
     );
 });
@@ -58,9 +49,6 @@ self.addEventListener('fetch', (e) => {
             console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
             return r || fetch(e.request).then((res) => {
                 return caches.open(cacheName).then((cache) => {
-                    // return caches.open(cacheName, {
-                    // mode: 'no-cors'
-                    // }).then((cache) => {
                     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
                     cache.put(e.request, res.clone());
                     return res;
